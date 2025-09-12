0161ecd307ba03e65859f948493649b80bcc4b8c
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-09-12 15:07:00 +0300

    refactor: clean up and implement async setup and unload for TISControl integration

--- __init__.py
+++ __init__.py
@@ -2,35 +2,55 @@
 
 from __future__ import annotations
 
-# import logging
-# import os
-# import aiofiles, json
-# import io
-# from attr import dataclass
+import logging
+from attr import dataclass
+from TISApi.api import TISApi
+from TISApi.Protocols.udp.ProtocolHandler import TISProtocolHandler
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
 
-DOMAIN = "tis_integration"
-
-
-# @dataclass
-# class TISData:
-#     """TISControl data stored in the ConfigEntry."""
-#     api: TISApi
+from .const import DEVICES_DICT, DOMAIN
 
 
-PLATFORMS: list[Platform] = [
-    Platform.SWITCH,
-]
+@dataclass
+class TISData:
+    """TISControl data stored in the ConfigEntry."""
+
+    api: TISApi
 
 
-async def async_setup_entry(hass, entry):
+PLATFORMS: list[Platform] = [Platform.SWITCH]
+type TISConfigEntry = ConfigEntry[TISApi]
+protocol_handler = TISProtocolHandler()
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: TISConfigEntry) -> bool:
     """Set up TISControl from a config entry."""
-    pass
+    tis_api = TISApi(
+        port=int(entry.data["port"]),
+        hass=hass,
+        domain=DOMAIN,
+        devices_dict=DEVICES_DICT,
+    )
+    entry.runtime_data = TISData(api=tis_api)
+
+    try:
+        await tis_api.connect()
+    except ConnectionError as e:
+        logging.error(f"Failed to connect: {e}")
+        raise ConfigEntryNotReady from e
+
+    # add the tis api to the hass data
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
 
 
-async def async_unload_entry(hass, entry):
+async def async_unload_entry(hass: HomeAssistant, entry: TISConfigEntry) -> bool:
     """Unload a config entry."""
-    pass
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        return unload_ok
+
+    return False
