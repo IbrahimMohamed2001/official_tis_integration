0a6eab691c8b0d6ae9d9bc97abd3af46e25a85d8
IbrahimMohamed2001 <ibrahim.hassan01@eng-st.cu.edu.eg>
2025-12-30 23:10:57 +0200

    validate the input first by trying to connect to tis and if done successfully save the tis_api instance to the entry's runtime data

--- config_flow.py
+++ config_flow.py
@@ -4,14 +4,15 @@ from __future__ import annotations
 
 import logging
 
+import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_PORT
 from homeassistant.core import callback
-import homeassistant.helpers.config_validation as cv
+from TISApi.api import TISApi
 
-from .const import DOMAIN
+from . import TISData
+from .const import DEVICES_DICT, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -27,9 +28,17 @@ class TISConfigFlow(ConfigFlow, domain=DOMAIN):
             await self.async_set_unique_id(unique_id)
             self._abort_if_unique_id_configured()
 
-            _LOGGER.info("Received user input %s", user_input)
+            is_valid = await self.validate_input(user_input)
 
-            return self.async_create_entry(title="TIS Control Bridge", data=user_input)
+            if not is_valid:
+                errors["base"] = "cannot_connect"
+                return self._show_setup_form(errors=errors)
+
+            _LOGGER.info("Received user input %s", user_input)
+            entry = self.async_create_entry(title="TIS Control Bridge", data=user_input)
+            entry.runtime_data = TISData(tis_api=self.tis_api)
+
+            return entry
 
         # If user_input is None (initial step), show the setup form
         return self._show_setup_form(errors=errors)
@@ -47,3 +56,18 @@ class TISConfigFlow(ConfigFlow, domain=DOMAIN):
             data_schema=schema,
             errors=errors if errors else {},
         )
+
+    async def validate_input(self, data: dict) -> None:
+        """Validate the user input allows us to connect."""
+        tis_api = TISApi(
+            port=int(data[CONF_PORT]),
+            domain=DOMAIN,
+            devices_dict=DEVICES_DICT,
+        )
+        try:
+            await tis_api.connect()
+            self.tis_api = tis_api
+            return True
+        except ConnectionError as e:
+            _LOGGER.error("Failed to connect: %s", e)
+            return False
